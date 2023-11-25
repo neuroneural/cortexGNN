cd497ca66c250aa06655ecac7f0e9dfa9c26c775
William Ashbee <wsashbee@gmail.com>
2023-11-25 19:51:30 +0000

    template OR white input

--- data/dataload.py
+++ data/dataload.py
@@ -4,8 +4,11 @@ import torch
 from tqdm import tqdm
 import nibabel as nib
 from torch.utils.data import Dataset
+import re
 
-
+from pytorch3d.io import load_objs_as_meshes
+from pytorch3d.structures import Meshes
+from utils import compute_normal, save_mesh_obj
 """
 volume: brain MRI volume
 v_in: vertices of input white matter surface
@@ -15,14 +18,14 @@ f_gt: faces of ground truth pial surface
 """
 
 class BrainData():
-    def __init__(self, volume, v_in, v_gt, f_in, f_gt):
+    def __init__(self, volume, v_in, v_gt, f_in, f_gt,subject_id,fsWIn = True):
         self.v_in = torch.Tensor(v_in)
         self.v_gt = torch.Tensor(v_gt)
         self.f_in = torch.LongTensor(f_in)
         self.f_gt = torch.LongTensor(f_gt)
         self.volume = torch.Tensor(volume).unsqueeze(0)
-        
-        
+        self.sub_id = int(subject_id)
+        self.fsWIn = fsWIn
 class BrainDataset(Dataset):
     def __init__(self, data):
         self.data = data
@@ -33,7 +36,7 @@ class BrainDataset(Dataset):
     def __getitem__(self, i):
         brain = self.data[i]
         return brain.volume, brain.v_gt, \
-                brain.f_gt, brain.v_in, brain.f_in
+                brain.f_gt, brain.v_in, brain.f_in, brain.sub_id
 
     
 def load_mri(path):
@@ -70,9 +73,28 @@ def load_surf(path):
     #================================================
 
     return v, f
+
+def load_surf_pytorch3D(path):
+    device = torch.device("cpu")
+    mesh = load_objs_as_meshes([path], device=device)
+    v = mesh.verts_list()[0].numpy()
+    f = mesh.faces_list()[0].numpy()
+           
+    v = v[:,[0,2,1]]*.9
+    v[:,0] = v[:,0] - 32
+    v[:,1] = - v[:,1] - 15
+    v[:,2] = v[:,2] - 32
+
+    # normalize to [-1, 1]
+    v = v + 128
+    v = (v - [96, 112, 96]) / 112
+    f = f.astype(np.int32)
+    #================================================
+
+    return v, f
     
     
-def load_data(data_path, hemisphere):
+def load_data(data_path, hemisphere,fsWIn=True):
     """
     data path: path of dataset
     """
@@ -82,22 +104,34 @@ def load_data(data_path, hemisphere):
     dataset = []
     
     for i in tqdm(range(len(subject_lists))):
-
+        
         subid = subject_lists[i]
+        
 
         # load brain MRI
         volume = load_mri(data_path + subid + '/mri/orig.mgz')
 
         # load ground truth pial surface
         v_gt, f_gt = load_surf(data_path + subid + '/surf/' + hemisphere + '.pial')
-#         v_gt, f_gt = load_surf(data_path + subid + '/surf/' + hemisphere + '.pial.deformed')
+        #v_gt, f_gt = load_surf(data_path + subid + '/surf/' + hemisphere + '.pial.deformed')
 
         # load input white matter surface
-        v_in, f_in = load_surf(data_path + subid + '/surf/' + hemisphere + '.white')
-#         v_in, f_in = load_surf(data_path + subid + '/surf/' + hemisphere + '.white.deformed')
+        v_in = None
+        f_in = None
+        if fsWIn:
+            v_in, f_in = load_surf(data_path + subid + '/surf/' + hemisphere + '.white')
+            #         v_in, f_in = load_surf(data_path + subid + '/surf/' + hemisphere + '.white.deformed')
+        else:#Todo:cleanup, debug
+            v_in, f_in = load_surf_pytorch3D("/pialnn/resources/mods/lh_white_smooth_140k_mod_pial.obj")
 
+        subid = str(subid)
+        numbers = re.findall('\d+', subid)[0]
+
+        print("numbers",numbers)  # Outputs: ['100', '20']
+
+        #subid = subid.replace("'", "").replace("\\", "").replace("(", "").replace(")", "")
         braindata = BrainData(volume=volume, v_gt=v_gt, f_gt=f_gt,
-                              v_in=v_in, f_in=f_in)
+                              v_in=v_in, f_in=f_in, subject_id=numbers,fsWIn=fsWIn)#Todo: make configurable
         dataset.append(braindata)
 
     return dataset
